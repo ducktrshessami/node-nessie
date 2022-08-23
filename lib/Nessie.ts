162cd22f62483cc8ec8b5b1b873e0785f1fa08ba
Josh Fish <jwee761@gmail.com>
2022-08-23 16:34:06 -0700

    model storage

--- lib/Nessie.ts
+++ lib/Nessie.ts
@@ -1,7 +1,9 @@
 import { BindParameters, Connection, getConnection, initOracleClient } from "oracledb"
+import Model from "./Model";
 
 export default class Nessie {
     private _connection: Connection | null;
+    readonly models: any;
 
     get connection() {
         return this._connection;
@@ -9,7 +11,12 @@ export default class Nessie {
 
     constructor(protected configuration: object) {
         this._connection = null;
+        this.models = {};
         initOracleClient(configuration);
+    }
+
+    addModels(...newModels: Array<typeof Model>) {
+        newModels.forEach(model => this.models[model.name] = model);
     }
 
     async connect() {
