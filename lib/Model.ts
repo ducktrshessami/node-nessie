c5735f0c6e7495bc4ac6231ae124a873e164ceef
Josh Fish <jwee761@gmail.com>
2022-08-23 21:00:32 -0700

    Model init

--- lib/Model.ts
+++ lib/Model.ts
@@ -1,18 +1,29 @@
 import Nessie from "./Nessie";
 import pluralize from "pluralize";
+import assert from "node:assert";
+import ModelInitError from "./errors/ModelInitError";
 
 export default abstract class Model {
-    private static _attributes: object;
+    private static _nessie?: Nessie;
+    private static _attributes: object | null = null;
 
     static get tableName() {
         return pluralize(this.name);
     }
 
-    constructor(public nessie: Nessie) {
+    constructor() {
         ;
     }
 
-    static init(attributes: object) {
+    static init(nessie: Nessie, attributes: object) {
+        this._nessie = nessie;
         this._attributes = attributes;
-    };
+    }
+
+    static initCheck() {
+        assert(this._nessie && this._attributes, new ModelInitError(`Model not initialized: ${this.name}`));
+        assert(this._nessie instanceof Nessie, new ModelInitError(`Invalid Nessie instance on model ${this.name}`));
+        assert(this._attributes, new ModelInitError(`Invalid attributes on model ${this.name}`));
+        return true;
+    }
 }
