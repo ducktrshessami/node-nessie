c0c78689cd25aadead559654df84b8a2c0acfdbd
Josh Fish <jwee761@gmail.com>
2022-08-23 21:47:59 -0700

    single instance for Nessie tests

--- test/Nessie.test.ts
+++ test/Nessie.test.ts
@@ -3,18 +3,20 @@ import assert from "assert";
 import { Nessie } from "../";
 
 describe("Nessie", function () {
+    let db: Nessie;
+
     before(function () {
         config();
-    });
-
-    it("connects given proper configuration", async function () {
-        const client = new Nessie({
+        db = new Nessie({
             libDir: process.env.DB_LIBDIR,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             connectionString: process.env.DB_CONNECTSTRING
         });
-        await client.connect();
-        assert(client.connection);
+    });
+
+    it("connects given proper configuration", async function () {
+        await db.connect();
+        assert(db.connection);
     });
 });
