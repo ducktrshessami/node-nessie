34fd27fb0323415ed60222e2cc828cee77b74b97
Josh Fish <jwee761@gmail.com>
2022-08-25 14:25:25 -0700

    updated Model tests

--- test/Model.test.ts
+++ test/Model.test.ts
@@ -1,6 +1,7 @@
 import assert from "assert";
 import { config } from "dotenv";
 import { Model, Nessie } from "../";
+import { DataTypes } from "../lib/utils/DataTypes";
 
 describe("Model", function () {
     let db: Nessie;
@@ -20,11 +21,23 @@ describe("Model", function () {
     });
 
     it("pluralizes class name for table name", function () {
-        assert.strictEqual(Foo.tableName, "Foos");
+        assert.strictEqual(Foo.tableName, "foos");
     });
 
     it("is accessible from Nessie instance after init", function () {
         Foo.init(db, {});
         assert.strictEqual(db.models.Foo, Foo);
+    });
+
+    it("syncs with db properly", async function () {
+        this.timeout(5000);
+        Foo.init(db, {
+            bar: {
+                type: DataTypes.STRING,
+                allowNull: false
+            }
+        });
+        await Foo.sync(true);
+        return db.execute(`SELECT * FROM "${Foo.tableName}"`);
     });
 });
