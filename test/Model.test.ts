46b00378c8e330e61242ac80a443c0d7f8a83fc6
Josh Fish <jwee761@gmail.com>
2022-08-24 23:33:43 -0700

    models structure test

--- test/Model.test.ts
+++ test/Model.test.ts
@@ -1,11 +1,30 @@
 import assert from "assert";
-import { Model } from "../";
+import { config } from "dotenv";
+import { Model, Nessie } from "../";
 
 describe("Model", function () {
+    let db: Nessie;
+
+    class Foo extends Model {
+        ;
+    }
+
+    before(function () {
+        config();
+        db = new Nessie({
+            libDir: process.env.DB_LIBDIR,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            connectionString: process.env.DB_CONNECTSTRING
+        });
+    });
+
     it("pluralizes class name for table name", function () {
-        class Foo extends Model {
-            ;
-        }
         assert.strictEqual(Foo.tableName, "Foos");
+    });
+
+    it("is accessible from Nessie instance after init", function () {
+        Foo.init(db, {});
+        assert.strictEqual(db.models.Foo, Foo);
     });
 });
