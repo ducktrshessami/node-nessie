b3b2b63952022027b0fec890017382b56ce5db23
Josh Fish <jwee761@gmail.com>
2022-08-27 15:48:28 -0700

    top level pseudocolumns enum

--- lib/utils/Constants.ts
+++ lib/utils/Constants.ts
@@ -2,3 +2,13 @@ export enum DataTypes {
     STRING = "VARCHAR(255)",
     NUMBER = "NUMBER"
 }
+
+export enum Pseudocolumns {
+    COLUMN_VALUE = "COLUMN_VALUE",
+    OBJECT_ID = "OBJECT_ID",
+    OBJECT_VALUE = "OBJECT_VALUE",
+    ORA_ROWSCN = "ORA_ROWSCN",
+    ROWID = "ROWID",
+    ROWNUM = "ROWNUM",
+    XMLDATA = "XMLDATA",
+}
