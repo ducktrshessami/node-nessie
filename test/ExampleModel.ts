9506e8d97b14ac1a2494547265096f7640a751c5
Josh Fish <jwee761@gmail.com>
2022-08-29 02:34:15 -0700

    update example model for new init syntax

--- test/ExampleModel.ts
+++ test/ExampleModel.ts
@@ -2,7 +2,7 @@ import { DataTypes, Model, Nessie } from "../";
 
 export default function ExampleModel(nessie: Nessie) {
     class Example extends Model { }
-    Example.init(nessie, {
+    Example.init({
         ID: {
             type: DataTypes.NUMBER,
             primaryKey: true
@@ -11,6 +11,6 @@ export default function ExampleModel(nessie: Nessie) {
             type: DataTypes.STRING,
             allowNull: false
         }
-    });
+    }, { nessie });
     return Example;
 }
