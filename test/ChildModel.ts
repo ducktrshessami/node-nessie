57afc4f10297bf42dd025d67d11d3cbfe72da9e3
Josh Fish <jwee761@gmail.com>
2022-08-31 10:01:34 -0700

    create test ChildModel

---
+++ test/ChildModel.ts
@@ -0,0 +1,12 @@
+import { DataTypes, Model, Nessie } from "../";
+
+export default function ChildModel(nessie: Nessie) {
+    class Child extends Model { }
+    Child.init({
+        id: {
+            type: DataTypes.NUMBER,
+            primaryKey: true
+        }
+    }, { nessie });
+    return Child;
+}
