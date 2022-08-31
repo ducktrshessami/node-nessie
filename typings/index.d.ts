f5fc22f2fb9f4e3f4a89755273027e84cb06f630
Josh Fish <jwee761@gmail.com>
2022-08-31 15:36:25 -0700

    build 30f8285

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -44,4 +44,22 @@ export class Model {
     destroy(options: any = {}): Promise<void>;
 }
 
-export * from "../lib/utils/Constants";
+export enum DataTypes {
+    STRING = "VARCHAR(255)",
+    NUMBER = "NUMBER"
+}
+
+export enum Pseudocolumns {
+    COLUMN_VALUE = "COLUMN_VALUE",
+    OBJECT_ID = "OBJECT_ID",
+    OBJECT_VALUE = "OBJECT_VALUE",
+    ORA_ROWSCN = "ORA_ROWSCN",
+    ROWID = "ROWID",
+    ROWNUM = "ROWNUM",
+    XMLDATA = "XMLDATA",
+}
+
+export enum OnDeleteBehavior {
+    CASCADE = "CASCADE",
+    SET_NULL = "SET NULL"
+}
