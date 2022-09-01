f28a9ef7330e051eea8e6e0523e76e3c776e0a28
Josh Fish <jwee761@gmail.com>
2022-08-31 23:39:08 -0700

    fix type declarations

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,18 +1,19 @@
 import { BindParameters, Connection, Pool, Result, Results } from "oracledb";
 
 export class Nessie {
+    protected configuration: any;
     readonly models: any;
     readonly pool: Pool | null;
 
-    constructor(protected configuration: any);
+    constructor(configuration: any);
 
     addModels(...newModels: Array<typeof Model>): void;
     define(name: string, attributes: any, options: any): typeof Model;
     initPool(): Promise<boolean>;
     connect(): Promise<Connection>;
-    execute(sql: string, bindParams: BindParameters = [], commit: boolean = false): Promise<Result<any>>;
-    executeMany(sql: string, bindParams: Array<BindParameters>, commit: boolean = false): Promise<Results<any>>;
-    sync(force: boolean = false): Promise<void>;
+    execute(sql: string, bindParams?: BindParameters, commit?: boolean): Promise<Result<any>>;
+    executeMany(sql: string, bindParams: Array<BindParameters>, commit?: boolean): Promise<Results<any>>;
+    sync(force?: boolean): Promise<void>;
     close(drainTime?: number): Promise<void>;
 }
 
@@ -30,19 +31,19 @@ export class Model {
     static init(attributes: any, options: any): void;
     static hasMany(other: typeof Model, options?: any): void;
     static belongsTo(other: typeof Model, options?: any): void;
-    static sync(force: boolean = false): Promise<void>;
+    static sync(force?: boolean): Promise<void>;
     static create(values: any, options: { select: false }): Promise<void>;
-    static create(values: any, options: any = {}): Promise<Model>;
-    static bulkCreate(values: Array<any>, options: any = {}): Promise<number>;
-    static findAll(options: any = {}): Promise<Array<Model>>;
-    static findOne(options: any = {}): Promise<Model?>;
-    static findByRowId(rowId: string): Promise<Model?>;
+    static create(values: any, options?: any): Promise<Model>;
+    static bulkCreate(values: Array<any>, options?: any): Promise<number>;
+    static findAll(options?: any): Promise<Array<Model>>;
+    static findOne(options?: any): Promise<Model | null>;
+    static findByRowId(rowId: string): Promise<Model | null>;
     static findOrCreate(options: any): Promise<[Model, boolean]>;
     static update(values: any, options: any): Promise<number>;
     static destroy(options: any): Promise<number>;
 
-    update(values: any, options: any = {}): Promise<this>;
-    destroy(options: any = {}): Promise<void>;
+    update(values: any, options?: any): Promise<this>;
+    destroy(options?: any): Promise<void>;
 }
 
 export enum DataTypes {
